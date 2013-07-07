077b4b76468c22f3fa88efd4cfb8922102bec3f5
joyicecloud <joyicecloud@gmail.com>
2013-07-06 19:02:37 -0700

    Restrict access to project actions to admins only

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,3 +1,13 @@
+module AuthHelpers
+	def sign_in(user)
+		session[:user_id] = user.id 
+	end
+end
+
+RSpec.configure do |c|
+	c.include AuthHelpers, type: :controller
+end
+
 module AuthenticationHelpers
   def sign_in_as!(user)
     visit '/signin'
