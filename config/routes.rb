4e0756ee00f5e26330b9a54084285502dbe8d57e
joyicecloud <joyicecloud@gmail.com>
2013-07-06 23:33:43 -0700

    Add admin namespaced users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Ticketee::Application.routes.draw do
-	resources :projects do
+	root :to => "projects#index"
+
+  resources :projects do
 	  resources :tickets
 	end
   
   resources :users
 	
-	root :to => "projects#index"
-
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
+
+  namespace :admin do
+    resources :users
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
